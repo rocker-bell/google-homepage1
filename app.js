15fd95d77b3906919497240fe835ccf79b0faa47
rocker bell <rockbell8@gmail.com>
2025-01-19 19:24:42 +0100

    Add modifications

---
+++ app.js
@@ -0,0 +1,15 @@
+const mode = document.getElementById("nav-icon1");
+const mail = document.getElementById("nav-icon2");
+const profile = document.getElementById("nav-icon3");
+
+mode.addEventListener("click", function () {
+    console.log("nav-icon1")
+})
+
+mail.addEventListener("click", function () {
+    console.log("nav-icon2")
+})
+
+profile.addEventListener("click", function () {
+    console.log("nav-icon3")
+})
