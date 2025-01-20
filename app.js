06ab04ad738ae04c6167649dad36d8b639664ae9
rocker bell <rockbell8@gmail.com>
2025-01-20 18:46:45 +0100

    Add box modal structure

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const mode = document.getElementById("nav-icon1");
 const mail = document.getElementById("nav-icon2");
 const profile = document.getElementById("nav-icon3");
 
+
+const modal = document.createElement("span");
+modal.classList = "modal";
+const box1 = document.createElement("div");
+box.classList.add = "box"
+box.classList = "box1";
+const box2 = document.createElement("div");
+box.classList = "box2";
+box.classList.add = "box"
+const box3 = document.createElement("div");
+box.classList = "box3";
+box.classList.add = "box"
+const box4 = document.createElement("div");
+box.classList = "box4";
+box.classList.add = "box"
+const box5 = document.createElement("div");
+box.classList = "box5";
+box.classList.add = "box"
+const box6 = document.createElement("div");
+box.classList = "box6"
+box.classList.add = "box"
+
+
+
 mode.addEventListener("click", function () {
     console.log("nav-icon1")
 })
